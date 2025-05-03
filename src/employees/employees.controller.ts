4083044e7f7e734a5740241ed3393542d62a15f7
lucasNasc <lucasfortunato328@gmail.com>
2025-05-03 20:40:31 -0300

    metodos para atualizacao de usuarios finalizados

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Post, UsePipes } from '@nestjs/common';
+import { Body, Controller, Param, Patch, Post, UsePipes } from '@nestjs/common';
 import { ParseToHourPipe } from 'src/common/pipes/hour-validation.pipe';
 import { CreateEmployeeDTO } from './dto/create-employee.dto';
+import { UpdateEmployeeAdminDTO } from './dto/update-employee-admin.dto';
+import { UpdateEmployeeDTO } from './dto/update-employee.dto';
 import { EmployeesService } from './employees.service';
 
 @Controller('employees')
@@ -11,5 +13,21 @@ export class EmployeesController {
   @UsePipes(ParseToHourPipe)
   Create(@Body() body: CreateEmployeeDTO) {
     return this.employeeService.Create(body);
+  }
+
+  @Patch('/update/self/:id')
+  UpdateSelf(
+    @Param('id') id: string,
+    @Body() updateEmployeeDTO: UpdateEmployeeDTO,
+  ) {
+    return this.employeeService.UpdateSelf(id, updateEmployeeDTO);
+  }
+
+  @Patch('/update/admin/:id')
+  UpdateAdmin(
+    @Param('id') id: string,
+    @Body() updateEmployeeAdminDTO: UpdateEmployeeAdminDTO,
+  ) {
+    return this.employeeService.UpdateAdmin(id, updateEmployeeAdminDTO);
   }
 }
